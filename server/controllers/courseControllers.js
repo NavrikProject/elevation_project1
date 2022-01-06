83fb7e6603b2f6e57a90cb08f8b406c1b99af3b6
Mahesh <b.mahesh311296@gmail.com>
2022-01-06 20:26:40 +0530

    fetching the courses frm backend

---
+++ server/controllers/courseControllers.js
@@ -0,0 +1,77 @@
+const connection = require("../dbConnection.js");
+
+exports.courseControllers = async (req, res, next) => {
+  const name = req.body.name;
+  const duration = req.body.duration;
+  const creationDate = req.body.creationDate;
+  const start = req.body.start;
+  const end = req.body.end;
+  const participants = req.body.participants;
+  const category = req.body.category;
+  const createdBy = req.body.createdBy;
+  const trainerName = req.body.trainerName;
+  const rating = req.body.rating;
+  const tags = req.body.tags;
+  try {
+    connection.query(
+      "INSERT INTO courses_dtls (course_name, course_duration, course_cr_date, course_start_dt, course_end_dt, course_participants, course_category, course_created_by, course_trainer_name, course_rating,course_tags) VALUES(?,?,?,?,?,?,?,?,?,?,?) ",
+      [
+        name,
+        duration,
+        creationDate,
+        start,
+        end,
+        participants,
+        category,
+        createdBy,
+        trainerName,
+        rating,
+        tags,
+      ],
+      (err, result) => {
+        if (result) {
+          res.send(result);
+        }
+        if (err) res.send(err.message);
+      }
+    );
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+exports.getCourseController = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    connection.query(
+      "SELECT * FROM courses_dtls WHERE course_id =?",
+      [id],
+      (err, result) => {
+        if (result) {
+          res.send(result);
+        }
+        if (err) {
+          res.send(err.message);
+        }
+      }
+    );
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+exports.allCourseControllers = (req, res, next) => {
+  try {
+    connection.query(
+      "SELECT * FROM courses_dtls ORDER BY course_id DESC LIMIT 10000 ;",
+      (err, result) => {
+        if (result) {
+          res.send(result);
+        }
+        if (err) {
+          res.send(err.message);
+        }
+      }
+    );
+  } catch (error) {
+    res.send(error.message);
+  }
+};
---
+++ server/routes/courseRoute.js
@@ -0,0 +1,15 @@
+const {
+  courseControllers,
+  getCourseController,
+  allCourseControllers,
+} = require("../controllers/courseControllers");
+
+const router = require("express").Router();
+
+router.post("/new-course", courseControllers);
+// sp course
+
+router.get("/full-course/:id", getCourseController);
+
+router.get("/all-courses", allCourseControllers)
+module.exports = router;
